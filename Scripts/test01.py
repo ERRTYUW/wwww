04ee16dc04f896dcd2d5cafd65cb8b753a667d27
ERRTYUW <951609532@qq.com>
2018-09-30 11:57:01 +0800

    12

---
+++ Scripts/test01.py
@@ -0,0 +1,17 @@
+import allure
+import pytest
+
+class Test01():
+    @allure.step("执行学院新增操作")
+    def test01(self):
+        print("test01被执行了")
+
+    @allure.step("执行学院更新操作")
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test02(self):
+        allure.attach("断言开始","断言是否更新")
+        print("test02被执行了")
+        allure.attach("断言结束","断言更新成功")
+    def test03(self):
+        print("test03被执行了")
